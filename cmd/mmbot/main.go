48ff5ada8787d1ed96307083bdbca0c895993ea1
ssccing <ssccing@users.noreply.github.com>
2026-09-02 11:17:28 +0800

    v0.3.11: frontend updates

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.3.10"
+var version = "0.3.11"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
