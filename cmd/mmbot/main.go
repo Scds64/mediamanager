ade1e07d9dfe9cfa8ee575327b38e848c64e75fd
ssccing <ssccing@users.noreply.github.com>
2026-09-02 09:10:07 +0800

    v0.3.10: frontend card title styling updates

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.3.9"
+var version = "0.3.10"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
