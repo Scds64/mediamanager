2a9f4c41c359ba25239f2b0961c542fa6f9ffd4a
Scds64 <834344946@qq.com>
2026-09-03 21:09:00 +0800

    v0.3.30: tmdb entry poster 2px border + rounded 14px (soft slate-600 in dark); bump version 0.3.30

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.3.29"
+var version = "0.3.30"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
