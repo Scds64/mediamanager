0a63c57b57d70577d0e06cab3fe312029f989d42
Scds64 <834344946@qq.com>
2026-09-03 17:42:27 +0800

    v0.3.29: mobile more-sheet starts closed (no auto-open on refresh); bump version 0.3.29

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.3.28"
+var version = "0.3.29"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
