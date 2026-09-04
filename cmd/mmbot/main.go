1aa0d77f60b7fd50cf57297c50ee5a61d5eb8f6e
Scds64 <834344946@qq.com>
2026-09-04 10:47:34 +0800

    v0.3.43: 搜索框增加日志搜索作用域并回车跳转日志页前端过滤；影视/整理搜索回车后清空输入框；影视搜索回车滚动定位到海报；版本号升至0.3.43

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.3.42"
+var version = "0.3.43"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
