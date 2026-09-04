3c5f0fef3c5f44c1fe807e7bd93fca1226a61edc
Scds64 <834344946@qq.com>
2026-09-04 17:12:56 +0800

    v0.3.52: 修正手机端手势滑动方向——系统页右滑出现侧边栏、左滑切后台；日志页右滑返回；侧边栏打开时左滑收起;版本号升至0.3.52

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.3.51"
+var version = "0.3.52"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
