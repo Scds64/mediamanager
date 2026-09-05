01f71a32e123b2cea28522be26183f0b32f9679d
Scds64 <834344946@qq.com>
2026-09-06 00:16:16 +0800

    v0.6.7: 卡片position:relative修复黑线+历史标题间距加大+手机端滚动黑线修复

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.6.6"
+var version = "0.6.7"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
