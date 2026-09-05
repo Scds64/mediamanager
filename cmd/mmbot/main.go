36aa0d1b928a1d41e035940778ef5b14c9c83196
Scds64 <834344946@qq.com>
2026-09-05 22:46:14 +0800

    v0.6.3: 修复多选按钮querySelector误选Emby刷新按钮，改用.multi-select-btn限定

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.6.2"
+var version = "0.6.3"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
