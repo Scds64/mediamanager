cac940def06e6db3846199b5d54fb5a5ff3ec302
Scds64 <834344946@qq.com>
2026-09-06 16:18:55 +0800

    v0.7.16 内存图Y轴20%边距+渐变顶部0.8

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.15"
+var version = "0.7.16"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
