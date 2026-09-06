63005bdef2a75ec1c4b3c4a3815991886728e1c8
Scds64 <834344946@qq.com>
2026-09-06 15:47:24 +0800

    v0.7.14 曲线平滑去过冲+Emby观看时长Y轴取整

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.13"
+var version = "0.7.14"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
