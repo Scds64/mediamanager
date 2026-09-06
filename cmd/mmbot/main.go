2ed5313b3d920edf34817f32c888d3ef6458b050
Scds64 <834344946@qq.com>
2026-09-06 08:36:11 +0800

    v0.7.2: 历史标题间距微调+文案优化

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.1"
+var version = "0.7.2"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
