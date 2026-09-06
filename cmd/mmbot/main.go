20d95fae09a360cdb1917659bf60e76f96b8514d
Scds64 <834344946@qq.com>
2026-09-06 09:55:51 +0800

    v0.7.5: 多选标题行高对齐

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.4"
+var version = "0.7.5"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
