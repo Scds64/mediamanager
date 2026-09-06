f269ff5d7a46ad324b282cf3e9f890d9a2c3c291
Scds64 <834344946@qq.com>
2026-09-06 18:11:44 +0800

    v0.7.18 内存图渐变改为中灰Zinc-400

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.17"
+var version = "0.7.18"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
