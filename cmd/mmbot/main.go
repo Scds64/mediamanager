f271247db7a060c99aed43cdc188c9c51d220060
Scds64 <834344946@qq.com>
2026-09-06 14:58:21 +0800

    v0.7.9 内存图Y轴自适应并去除网格线

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.8"
+var version = "0.7.9"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
