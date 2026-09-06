f861d6fcdc3cd5031698e971ab37ec133a55806a
Scds64 <834344946@qq.com>
2026-09-06 17:18:22 +0800

    v0.7.17 内存图Y轴最小跨度5MB+12%边距

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.16"
+var version = "0.7.17"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
