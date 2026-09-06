237a0145161d2f80da1ebf5c3b21bfd2733e02c0
Scds64 <834344946@qq.com>
2026-09-06 21:32:57 +0800

    v0.7.24 圆环配色：电影Zinc-800 + 电视剧Emerald-600

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.23"
+var version = "0.7.24"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
