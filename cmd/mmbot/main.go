8913ef6c4e1c21d8d5f250bb96910018362265c3
Scds64 <834344946@qq.com>
2026-09-06 15:02:14 +0800

    v0.7.10 修正版本号(0.8.0->0.7.10)

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.8.0"
+var version = "0.7.10"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
