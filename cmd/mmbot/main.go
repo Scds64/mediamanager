b0f5cc894a9338240c021f97e83c5b502d4235d2
Scds64 <834344946@qq.com>
2026-09-06 21:17:20 +0800

    v0.7.23 圆环相连端移至底部

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.22"
+var version = "0.7.23"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
