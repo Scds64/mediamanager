8f563da4be8a9aa03423a21002d92977880f4302
Scds64 <834344946@qq.com>
2026-09-06 15:09:31 +0800

    v0.7.11 内存图去线改纯渐变晕染

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.10"
+var version = "0.7.11"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
