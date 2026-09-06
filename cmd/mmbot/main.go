4c4f1209932a08aa5396a4a5391b342ae4b9d32e
Scds64 <834344946@qq.com>
2026-09-06 20:38:37 +0800

    v0.7.21 圆环优化：9px圆角端点 相连端重叠消除凸包

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.20"
+var version = "0.7.21"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
