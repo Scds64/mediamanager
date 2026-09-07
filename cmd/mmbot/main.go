0d3dae12691a1b5a9154abe649f5b201925f3e36
ssccing <ssccing@users.noreply.github.com>
2026-09-07 16:35:24 +0800

    v0.7.32: 卡片标题去掉前置小图标 字重统一600 订阅探索标题字号与卡片标题一致

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.31"
+var version = "0.7.32"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
