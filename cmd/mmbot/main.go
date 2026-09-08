cd7f1aa5516f4f22810cb66bcff651f2663f1b5a
Scds64 <834344946@qq.com>
2026-09-08 22:54:33 +0800

    v0.7.53: iOS聚焦输入框不再自动放大页面(缩放85%%时亦生效)

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.52"
+var version = "0.7.53"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
