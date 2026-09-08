d13eb26012f3a3cd7125b034dd5ab77388124d23
Scds64 <834344946@qq.com>
2026-09-08 13:49:35 +0800

    v0.7.48: 移动端字体调小一级 所有非中间海报点击均可切换

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.47"
+var version = "0.7.48"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
