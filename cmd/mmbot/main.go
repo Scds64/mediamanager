c13cbe7dbfedb84738805492c2f355afc162ccec
Scds64 <834344946@qq.com>
2026-09-08 13:30:21 +0800

    v0.7.47: 移动端仅订阅探索卡片出血满宽 其他卡片保持正常宽度

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.46"
+var version = "0.7.47"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
