96d61dc3d76adcaa2c9f5ce7785d7db2129538c7
Scds64 <834344946@qq.com>
2026-09-08 11:25:28 +0800

    v0.7.43: 移动端订阅探索海报高度调整为320px

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.42"
+var version = "0.7.43"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
