094e1fc0eed298770dd2de0125cdc731eb48c2bf
Scds64 <834344946@qq.com>
2026-09-08 13:08:54 +0800

    v0.7.46: 订阅探索卡片移到媒体信息前 移动端占满宽

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.45"
+var version = "0.7.46"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
