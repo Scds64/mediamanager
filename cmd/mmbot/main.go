11613e01cb67b9e71f086f08caf687d24dc9b3f7
ssccing <ssccing@users.noreply.github.com>
2026-09-08 18:08:54 +0800

    v0.7.51: 媒体信息只留上次扫描(圆环下方) 内存数值移入图中央 两卡正方形且移动端并排

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.50"
+var version = "0.7.51"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
