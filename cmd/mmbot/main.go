a8a87e1215a61e2834c80fee39c0bbd1480709d4
Scds64 <834344946@qq.com>
2026-09-08 11:49:21 +0800

    v0.7.44: 海报卡片圆角增大至16px

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.43"
+var version = "0.7.44"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
