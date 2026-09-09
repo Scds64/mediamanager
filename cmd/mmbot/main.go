c820ff08fcc5ca537e20e1cd78b97ee958999a3c
Scds64 <834344946@qq.com>
2026-09-09 11:35:20 +0800

    feat: 媒体信息卡重排版（容量居中/电影电视剧计数左下角/上次扫描底部居中）[v0.7.58]

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.7.57"
+var version = "0.7.58"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
