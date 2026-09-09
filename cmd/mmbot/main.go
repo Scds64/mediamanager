62d450806b17b4f8ced80ace10072fb630afd1ae
Scds64 <834344946@qq.com>
2026-09-09 21:59:57 +0800

    fix: 手机端海报详情演员区固定每行 5 个，10 人两行满排不再留白 [v0.8.7]

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.8.6"
+var version = "0.8.7"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
