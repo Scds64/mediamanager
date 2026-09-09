78c7e4a11b320f9529347a7056526b702ec1fdc1
Scds64 <834344946@qq.com>
2026-09-09 17:20:03 +0800

    fix: iOS PWA 禁用页面缩放（viewport user-scalable/maximum-scale、touch-action、输入框 16px）[v0.8.2]

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.8.1"
+var version = "0.8.2"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
