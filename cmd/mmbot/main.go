e8408949af7b987459d26a093e922e97a3ead28c
Scds64 <834344946@qq.com>
2026-09-14 20:03:15 +0800

    fix: 订阅探索海报mask改为前80%保持不透明、仅最下20%快速淡出 [v0.8.30]

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.8.29"
+var version = "0.8.30"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
