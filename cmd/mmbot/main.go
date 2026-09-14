dbbfe55f25a7641d833a92046ca5009c6f8befea
Scds64 <834344946@qq.com>
2026-09-14 19:28:24 +0800

    fix: 手机端后台页支持手指右滑返回系统页（补齐双向滑动手势） [v0.8.29]

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.8.28"
+var version = "0.8.29"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
