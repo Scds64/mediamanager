c102597238cc481a8b42b88dd010a295d4c00ae1
Scds64 <834344946@qq.com>
2026-09-14 12:38:06 +0800

    [v0.8.26] 手机端海报向下出血到搜索框：搜索框浮于海报淡出处，徽标视觉位置不变

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.8.25"
+var version = "0.8.26"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
