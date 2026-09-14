570664b7b196fdf67be5765811b4e7d4f747944a
Scds64 <834344946@qq.com>
2026-09-14 21:28:47 +0800

    v0.8.32: 订阅探索海报mask改为上方60%高度不透明、下方40%高度渐变透明 [v0.8.32]

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.8.31"
+var version = "0.8.32"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
