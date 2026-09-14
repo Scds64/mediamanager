78252a53ef07d23a2bab76edee55b4409d232fca
Scds64 <834344946@qq.com>
2026-09-14 11:46:06 +0800

    [v0.8.25] 手机端海报淡出遮罩优化：中部更长保持高清，底部平滑淡化避免生硬分界

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.8.24"
+var version = "0.8.25"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
