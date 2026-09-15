9ef9e1dcb62e48f8f08ce2532a0a84cbab2a9966
Scds64 <834344946@qq.com>
2026-09-15 10:57:10 +0800

    v0.8.38: 汉堡按钮改为三条横线动画，点击展开时收成向下的V [v0.8.38]

--- cmd/mmbot/main.go
+++ cmd/mmbot/main.go
@@ -214,7 +214,7 @@ func initLogging(path string) {
 	log.SetPrefix("")
 }
 
-var version = "0.8.37"
+var version = "0.8.38"
 
 func fatalf(format string, args ...any) {
 	fmt.Fprintf(os.Stderr, format+"\n", args...)
